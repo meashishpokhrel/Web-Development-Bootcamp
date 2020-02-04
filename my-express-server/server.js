76ec7d424f5005ac4b8e8af0a872b8de686c6fb0
meashishpokhrel <meashishpokhrel@gmail.com>
2020-02-04 16:28:49 +0545

    server side Express basic

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const app= express();
 
+app.get('/', function(request,response){
+    response.send("hello world");
+})
+app.get('/contact', function(request,response){
+    response.send("Contact me @ ashish.com.np");
+})
+
+app.get('/about', function(request,response){
+    response.send("my bio will be lsited here");
+})
 app.listen(3000, function(){
     console.log("server has started on 30000");
 });
