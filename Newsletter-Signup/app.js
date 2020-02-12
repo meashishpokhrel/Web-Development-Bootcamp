425039e3693a0e82fe2589dad52fde8e445628b0
meashishpokhrel <meashishpokhrel@gmail.com>
2020-02-12 17:18:28 +0545

    Posting Data to Mailchimp Servers

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -14,9 +14,46 @@ app.post("/",function(req,res){
     var fname = (req.body.fn);
     var lname = (req.body.ln);
     var email = (req.body.email);
-    console.log(fname, lname, email);
-});  
+    //console.log(fname, lname, email);
+ 
+        var data={
+        members:[
+        {
+            email_address: email,
+            status:"subscribed",
+            merge_fields:{
+                FNAME: fname,
+                LNAME: lname
+            }
 
+
+        }
+    ]
+    };
+
+
+var jsonData=JSON.stringify(data);
+
+
+var options={
+    url: "https://us4.api.mailchimp.com/3.0/lists/895b379718",
+    method: "POST",
+    headers:{
+
+        "Authorization":"boltar c00364eb700ba3661f66f6c9b7125192-us4"
+    },
+    body:jsonData,
+};
+request(options, function(err, resp, body){
+    if (err){
+        console.log(err);
+    }
+    else{
+        console.log(resp.statusCode);
+    }
+});
+
+}); 
   
 app.listen(3000, function(){
     console.log("listening 3000 port");
@@ -24,3 +61,4 @@ app.listen(3000, function(){
 
 
 //    c00364eb700ba3661f66f6c9b7125192-us4
+//  895b379718
