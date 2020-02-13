02920e2c1f2851938bffce540fdfc1153a7a5424
meashishpokhrel <meashishpokhrel@gmail.com>
2020-02-13 15:58:07 +0545

    First EJS Template

--- todolist/todolist-v1/app.js
+++ todolist/todolist-v1/app.js
@@ -3,21 +3,42 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+
+// app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req,res){
     var today = new Date();
     var currentDay = today.getDay();
+    var day="";
 
-    if (currentDay === 6 || currentDay===0){
-        res.write("It;s the Weekenf Enjoy");
+    if (currentDay === 0){
+        day = "Sunday";
     }
-    else{
-        res.sendFile(__dirname + "/index.html");
+    else if(currentDay === 1){
+        day ="Monday";
     }
+    else if(currentDay === 2){
+        day ="Tuesday";
+    }
+    else if(currentDay === 3){
+        day ="Wedneday";
+    }
+    else if(currentDay === 4){
+        day ="Thursday";
+    }
+    else if(currentDay === 5){
+        day ="Friday";
+    }
+    else if(currentDay === 6){
+        day ="Saturday";
+    }
+
+    res.render("list",{kindofDay:day});
 });
 
 
-app.listen(3001, function(){
+app.listen(3000, function(){
     console.log("lsiting on 3000");
 });
