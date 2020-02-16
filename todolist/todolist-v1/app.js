ba295486080eab0c8fa5b3e8bb1be92c38c873af
meashishpokhrel <meashishpokhrel@gmail.com>
2020-02-16 21:51:50 +0545

    Layout Added (Header + Footer)

--- todolist/todolist-v1/app.js
+++ todolist/todolist-v1/app.js
@@ -4,12 +4,13 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items=["Buy Food","Cook Food","Eat Food"];
+let workList=[];
 
 app.set('view engine', 'ejs');
 
 
  app.use(bodyParser.urlencoded({extended: true}));
- app.use(express.static("public"));
+ app.use(express.static("public")); 
 
 app.get("/",function(req,res){
     let today = new Date();
@@ -21,13 +22,30 @@ app.get("/",function(req,res){
     };
 
     let day= today.toLocaleDateString("en-US",option);
-    res.render("list",{kindofDay:day,newlistItem:items});
+    res.render("list",{listTitle:day,newlistItem:items});
 });
 
 app.post("/", function(req,res){
     let item=(req.body.text);
-    items.push(item);
-    res.redirect("/");
+
+    if (req.body.list === "Work List"){
+        workList.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+    
+});
+
+app.get("/work",function(req,res){
+    res.render("list",{listTitle:"Work List",newlistItem:workList})
+});
+
+
+app.get("/about",function(req,res){
+    res.render("about");
 });
 
 app.listen(3000, function(){
