5c007d406c961b248215a919c657976c95c86b7d
meashishpokhrel <meashishpokhrel@gmail.com>
2020-05-17 15:31:36 +0545

    Mongoose Intro + Basic Use

---
+++ FruitsProject/app.js
@@ -0,0 +1,62 @@
+
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true , useUnifiedTopology: true});
+
+const fruitSchema = new mongoose.Schema ({
+    name: String,
+    rating: Number,
+    review: String
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit ({
+    name: "Apple",
+    rating: 4,
+    review: "Very nice"
+});
+
+//fruit.save();
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    Age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person ({
+    name: "Ashish",
+    Age: 18
+});
+
+person.save();
+
+const kiwi = new Fruit ({
+    name: "KIWI",
+    rating: 4,
+    review: "Very nice"
+});
+
+const banana = new Fruit ({
+    name: "banana",
+    rating: 4,
+    review: "Very nice"
+});
+
+const orange = new Fruit ({
+    name: "Orange",
+    rating: 4,
+    review: "Very nice"
+});
+
+Fruit.insertMany([kiwi,banana,orange],function(err){
+    if (err){
+        console.log(err);
+    }
+    else{
+        console.log("sucees");
+    }    
+
+});
