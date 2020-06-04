0bb1d41db9398de4bb6b79869667e92d7c9d15bb
meashishpokhrel <meashishpokhrel@gmail.com>
2020-06-04 20:32:27 +0545

    Server Setup

---
+++ RESTful-API/app.js
@@ -0,0 +1,29 @@
+//jshint esversion:6
+
+const express = require ("express");
+const bodyParser = require("body-parser");
+const ejs = require ("ejs");
+const mongoose = require ("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+
+ app.use(bodyParser.urlencoded({extended: true}));
+ app.use(express.static("public")); 
+
+ mongoose.connect("mongodb://localhost:27017/wikiDB",{ useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false});
+
+ const articleSchema = {
+    title: String,
+    content: String
+};
+
+const article = mongoose.model("article",articleSchema);
+
+
+
+app.listen(3000, function(){
+    console.log("listening on 3000 port:");
+});
